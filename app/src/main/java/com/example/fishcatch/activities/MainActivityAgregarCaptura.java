57777ab49663a7ca8ae1c1051d33407e18b56dc9
BartolomeGarcia <bgarciar11@iesarroyoharnina.es>
2025-04-22 20:22:19 +0200

    Agregar captura layout hecho

--- app/src/main/java/com/example/fishcatch/activities/MainActivityAgregarCaptura.java
+++ app/src/main/java/com/example/fishcatch/activities/MainActivityAgregarCaptura.java
@@ -1,17 +1,30 @@
 package com.example.fishcatch.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fishcatch.R;
 
-public class MainActivityAgregarCaptura extends AppCompatActivity {
+public class MainActivityAgregarCaptura extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_agregar_captura);
+
+    }
+
+    //Para el Spinner de Especies
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
